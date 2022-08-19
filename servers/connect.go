f0c666ce84a1f3e66f685509ca66ce8e182c9804
bryan sun <hitsjt@gmail.com>
2022-08-19 19:22:15 +0800

    set pong handler

--- servers/connect.go
+++ servers/connect.go
@@ -13,6 +13,15 @@ import (
 const (
 	// 最大的消息大小
 	maxMessageSize = 8192
+
+	// Time allowed to write a message to the peer.
+	//writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	//pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	//pingPeriod = (pongWait * 9) / 10
 )
 
 type Controller struct {
@@ -43,6 +52,12 @@ func (c *Controller) Run(w http.ResponseWriter, r *http.Request) {
 
 	//设置读取消息大小上线
 	conn.SetReadLimit(maxMessageSize)
+
+	//收到pong后
+	conn.SetPongHandler(func(appData string) error {
+		//fmt.Println("got pong data ", time.Now().String())
+		return nil
+	})
 
 	//解析参数
 	systemId := r.FormValue("systemId")
